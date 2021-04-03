aa4f8291169caaeef59f8bb413ca4213170172db
k1rakishou <kez1911@protonmail.com>
2021-04-03 13:32:36 +0300

    v0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
     applicationId = "com.github.mangaloid.client"
     minSdk = 21
     targetSdk = 30
-    versionCode = 1
-    versionName = "1.0"
+    versionCode = 2
+    versionName = "0.2"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
